c6f1ea14f5180fa3c802485d569e4d5c271d9669
abhil <abhilash.kamboj@gmail.com>
2021-09-14 11:28:31 +0200

    payload change

--- payload.js
+++ payload.js
@@ -3,7 +3,7 @@ function payload(name,lastname,age,dep) {
     return {
             first_name: name,
             last_name: lastname,
-            age: 40,
+            age: age,
             department: dep
     };
 }
